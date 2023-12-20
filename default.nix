e8be1baf471e40b9d94059c45cb699eba701fa9a
Jesse <jesseholzman96@gmail.com>
2023-12-20 17:33:30 -0500

    Get rid of default.nix for now

--- default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.rustPlatform.buildRustPackage rec {
-    pname = "procedural-animation-bevy";
-    version = "0.1";
-    cargoLock.lockFile = ./Cargo.lock;
-    src = pkgs.lib.cleanSource ./.;
-}
