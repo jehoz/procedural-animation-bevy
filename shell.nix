b73c78f81c055135798ec2779654e3b21184730a
Jesse <jesseholzman96@gmail.com>
2023-12-20 17:33:47 -0500

    Add bevy dependency

--- shell.nix
+++ shell.nix
@@ -1,9 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-    inputsFrom = [ (pkgs.callPackage ./default.nix {}) ];
-    buildInputs = with pkgs; [
-      rust-analyzer
-      rustfmt
-      clippy
-    ];
+with pkgs;
+pkgs.mkShell rec {
+  nativeBuildInputs = [
+    pkg-config
+    cargo rustc
+  ];
+  buildInputs = [
+    udev alsa-lib vulkan-loader
+    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # x11 deps
+    libxkbcommon wayland # wayland deps
+
+    rust-analyzer rustfmt clippy
+  ];
+
+  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 }
