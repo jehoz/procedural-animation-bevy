e790753288311b178486c5f14af31e0502cb5109
Jesse <jesseholzman96@gmail.com>
2023-12-27 14:22:11 -0500

    Make creature a sequence of connected body segments

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,26 @@ use bevy::prelude::*;
 use rand::prelude::*;
 
 #[derive(Component)]
+struct BodySegment {
+    entity: Option<Entity>,
+    distance: f32,
+}
+
+impl BodySegment {
+    fn new() -> Self {
+        BodySegment {
+            entity: None,
+            distance: 0.3,
+        }
+    }
+}
+
+#[derive(Component)]
 struct Creature {
     move_speed: f32,
     turn_speed: f32,
     target_position: Vec3,
+    body_segments: Vec<BodySegment>,
 }
 
 impl Creature {
@@ -14,6 +30,7 @@ impl Creature {
             move_speed: 2.0,
             turn_speed: 3.0,
             target_position: Vec3::ZERO,
+            body_segments: Vec::new(),
         }
     }
 }
@@ -54,53 +71,84 @@ fn setup(
         ..default()
     });
 
-    // creature (sphere)
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Box {
-                min_x: -0.25,
-                max_x: 0.25,
-                min_y: -0.25,
-                max_y: 0.25,
-                min_z: -0.5,
-                max_z: 0.5,
-            })),
-            material: materials.add(Color::WHITE.into()),
-            transform: Transform::IDENTITY,
-            ..default()
-        },
-        Creature::new(),
-    ));
+    // creature
+    let mut creature = Creature::new();
+    for i in 0..8 {
+        let mut segment = BodySegment::new();
+        segment.entity = Some(
+            commands
+                .spawn(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Box {
+                        min_x: -0.1,
+                        max_x: 0.1,
+                        min_y: -0.1,
+                        max_y: 0.1,
+                        min_z: -0.1,
+                        max_z: 0.1,
+                    })),
+                    material: materials.add(Color::WHITE.into()),
+                    transform: Transform::IDENTITY.with_translation(Vec3::new(
+                        0.0,
+                        0.25,
+                        (i as f32) * segment.distance,
+                    )),
+                    ..default()
+                })
+                .id(),
+        );
+        creature.body_segments.push(segment);
+    }
+    commands.spawn(creature);
 }
 
 fn move_creatures(
     mut gizmos: Gizmos,
-    mut query: Query<(&mut Transform, &mut Creature)>,
+    mut creatures: Query<&mut Creature>,
+    mut transforms: Query<&mut Transform>,
     time: Res<Time>,
 ) {
-    for (mut transform, mut creature) in &mut query {
-        if transform.translation.distance(creature.target_position) < 0.5 {
+    for mut creature in &mut creatures {
+        let mut head = transforms
+            .get_mut(creature.body_segments[0].entity.unwrap())
+            .unwrap();
+
+        // if reached target position, choose a new one randomly
+        if head.translation.distance(creature.target_position) < 0.5 {
             creature.target_position = Vec3 {
                 x: (random::<f32>() * 10.0) - 5.0,
-                y: transform.translation.y,
+                y: head.translation.y,
                 z: (random::<f32>() * 10.0) - 5.0,
             };
         }
 
         gizmos.circle(creature.target_position, Vec3::Y, 0.25, Color::RED);
 
-        let target_dir = (creature.target_position - transform.translation).normalize();
-        let rquat = Quat::from_rotation_arc(transform.forward(), target_dir);
-        let (_, mut y_rot, _) = rquat.to_euler(EulerRot::XYZ);
-        if y_rot < 0.0 {
-            y_rot = f32::max(y_rot, -1.0 * creature.turn_speed * time.delta_seconds());
-        } else {
-            y_rot = f32::min(y_rot, creature.turn_speed * time.delta_seconds());
+        // turn head towards target
+        let target_dir = (creature.target_position - head.translation).normalize();
+        let rot = Quat::from_rotation_arc(head.forward(), target_dir);
+        let (_, mut y_rot, _) = rot.to_euler(EulerRot::XYZ);
+        let max_turn = creature.turn_speed * time.delta_seconds();
+        y_rot = y_rot.clamp(-max_turn, max_turn);
+        head.rotate(Quat::from_rotation_y(y_rot));
+
+        // move head forward
+        let movement = head.forward() * creature.move_speed * time.delta_seconds();
+        head.translation += movement;
+
+        // move each body segment towards the one ahead of it
+        for i in 1..creature.body_segments.len() {
+            let segment = &creature.body_segments[i];
+            let [parent, mut current] = transforms.many_mut([
+                creature.body_segments[i - 1].entity.unwrap(),
+                segment.entity.unwrap(),
+            ]);
+            gizmos.line(parent.translation, current.translation, Color::WHITE);
+
+            current.look_at(parent.translation, Vec3::Y);
+
+            let dist = parent.translation.distance(current.translation);
+            let movement = current.forward() * (dist - segment.distance);
+            current.translation += movement;
         }
-
-        transform.rotate(Quat::from_rotation_y(y_rot));
-
-        let movement = transform.forward() * creature.move_speed * time.delta_seconds();
-        transform.translation += movement;
     }
 }
